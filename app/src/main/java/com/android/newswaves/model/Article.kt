63c7c22109e688bc02f1af0b4f7b7c3440557d06
Paulus-Lawrenzo <paulus.martuatambunan@gmail.com>
2024-02-13 04:58:40 +0700

    Setup Room & DAO, Modify Article

--- app/src/main/java/com/android/newswaves/model/Article.kt
+++ app/src/main/java/com/android/newswaves/model/Article.kt
@@ -4,7 +4,10 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.io.Serializable
 
+@Entity(tableName = "article")
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null,
     val author: String?,
     val content: String?,
     val description: String?,
@@ -13,4 +16,13 @@ data class Article(
     val title: String?,
     val url: String?,
     val urlToImage: String?
-)
+) : Serializable {
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        if (url.isNullOrEmpty()) {
+            result = 31 * result + url.hashCode()
+        }
+        return result
+    }
+}
