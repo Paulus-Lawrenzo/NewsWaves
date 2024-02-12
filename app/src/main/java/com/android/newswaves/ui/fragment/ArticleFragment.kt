87d246cc4ad09ecd5a100a708208ccb565c019c3
Paulus-Lawrenzo <paulus.martuatambunan@gmail.com>
2024-02-13 06:01:38 +0700

    Showing News in the WebView

--- app/src/main/java/com/android/newswaves/ui/fragment/ArticleFragment.kt
+++ app/src/main/java/com/android/newswaves/ui/fragment/ArticleFragment.kt
@@ -5,15 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.navigation.fragment.navArgs
 import com.android.newswaves.R
 import com.android.newswaves.ui.activity.NewsActivity
 import com.android.newswaves.ui.viewmodel.NewsViewModel
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
     lateinit var viewModel: NewsViewModel
+    val args: ArticleFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
+
+        val article = args.article
+        val webView: WebView = view.findViewById(R.id.webView)
+        webView.apply {
+            webView.webViewClient = WebViewClient()
+            article.url?.let { webView.loadUrl(it) }
+        }
     }
 }
--- app/src/main/java/com/android/newswaves/ui/fragment/SavedNewsFragment.kt
+++ app/src/main/java/com/android/newswaves/ui/fragment/SavedNewsFragment.kt
@@ -5,15 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.android.newswaves.R
+import com.android.newswaves.adapter.NewsAdapter
 import com.android.newswaves.ui.activity.NewsActivity
 import com.android.newswaves.ui.viewmodel.NewsViewModel
 
 class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
     lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+    lateinit var rvSavedNews: RecyclerView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
+
+        setupRecyclerView()
+
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("article", it)
+            }
+            findNavController().navigate(
+                R.id.action_savedNewsFragment_to_articleFragment,
+                bundle
+            )
+        }
+    }
+
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter()
+        rvSavedNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 }
