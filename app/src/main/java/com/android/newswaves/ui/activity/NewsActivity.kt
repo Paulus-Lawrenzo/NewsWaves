ac0f1dc3955cea8017eeb61c1b054e3e0c88e65e
Paulus-Lawrenzo <paulus.martuatambunan@gmail.com>
2024-02-13 04:39:25 +0700

    Setup Navigation

--- app/src/main/java/com/android/newswaves/ui/activity/NewsActivity.kt
+++ app/src/main/java/com/android/newswaves/ui/activity/NewsActivity.kt
@@ -2,11 +2,22 @@ package com.android.newswaves.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.android.newswaves.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class NewsActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
 }
