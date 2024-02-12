9df8f009a8a8053c6c1726f561210b8a94549eeb
Paulus-Lawrenzo <paulus.martuatambunan@gmail.com>
2024-02-13 04:22:53 +0700

    Setup All The Layouts with All The Necessary Components and Files

--- app/src/main/java/com/android/newswaves/ui/activity/NewsActivity.kt
+++ app/src/main/java/com/android/newswaves/ui/activity/NewsActivity.kt
@@ -1,11 +1,12 @@
-package com.android.newswaves
+package com.android.newswaves.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.newswaves.R
 
-class MainActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news)
     }
 }
